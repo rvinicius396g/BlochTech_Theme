cf88693dfbba563ecd76ad449671b9558a7b69b3
rvinicius396g <roberto396g@hotmail.com>
2020-09-02 14:18:42 -0300

    customização do header

--- header.php
+++ header.php
@@ -24,14 +24,7 @@
     </a>
         <?php
         wp_nav_menu( array(
-            'theme_location'    => 'primary',
-            'depth'             => 2,
-            'container'         => 'div',
-            'container_class'   => 'collapse navbar-collapse',
-            'container_id'      => 'bs-example-navbar-collapse-1',
-            'menu_class'        => 'nav navbar-nav',
-            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-            'walker'            => new WP_Bootstrap_Navwalker(),
+            'theme_location'    => 'menu-navegacao-top' 
         ) );
         ?>
     </div>
